4c886a3c7feaed7b2f13bf09575b756b54c99483
tekostyshyn <tekostyshyn@gmail.com>
2023-04-14 15:37:15 +0300

    first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Title from './Title';
+import Buttons from './Buttons';
+import Statistics from './Statistics';
+
+export class App extends Component {
+  state = {
+    good: 5,
+    neutral: 10,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    return ( Math.round(good / (this.countTotalFeedback()) * 100));
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div>
+        <Title text="Please leave feedback" />
+        <Buttons data={this.state}></Buttons>
+        <Title text="Statistics" />
+        <Statistics
+          good={good}
+          bad={bad}
+          neutral={neutral}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+        />
+      </div>
+    );
+  }
+}
